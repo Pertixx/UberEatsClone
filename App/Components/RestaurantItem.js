20d4dff6371d2b460e49b9766ecba9fa64337709
Agustin Perticaro <pertix2000@gmail.com>
2021-11-20 12:57:13 -0300

    Finished restaurant component

--- App/Components/RestaurantItem.js
+++ App/Components/RestaurantItem.js
@@ -1,14 +1,60 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import { RFValue } from 'react-native-responsive-fontsize'
+import { FontAwesome } from '@expo/vector-icons'
 
-const RestaurantItem = () => {
+const RestaurantItem = ({info}) => {
   return (
-    <View>
-      <Text>RestaurantItem</Text>
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Image
+          source={{ uri: info.image }}
+          style={styles.image}
+          resizeMode='cover'
+        />
+        <TouchableOpacity style={styles.heartButton}>
+          <FontAwesome name="heart-o" size={24} color="white" />
+        </TouchableOpacity>
+        <View style={styles.infoContainer}>
+          <View>
+            <Text style={{fontWeight: 'bold', fontSize: RFValue(14)}}>{info.name}</Text>
+            <Text style={{color: '#adb5bd'}}>{info.time}</Text>
+          </View>
+          <View style={styles.reviewContainer}>
+            <Text>{info.rating}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default RestaurantItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    backgroundColor: '#fff',
+    padding: RFValue(10),
+  },
+  image: {
+    width: '100%',
+    height: RFValue(150),
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: RFValue(10),
+  },
+  reviewContainer: {
+    backgroundColor: '#eee',
+    padding: RFValue(8),
+    borderRadius: RFValue(50),
+  },
+  heartButton: {
+    position: 'absolute',
+    right: RFValue(10),
+    top: RFValue(10),
+  },
+})
--- App/Screens/Home/index.js
+++ App/Screens/Home/index.js
@@ -7,24 +7,57 @@ import RestaurantItem from '../../Components/RestaurantItem'
 import SearchBar from '../../Components/SearchBar'
 
 const HomeScreen = () => {
+
+  const restaurants = [
+    {
+      id: 123,
+      image: 'https://cdn.pixabay.com/photo/2016/11/18/15/31/cooking-1835369_1280.jpg',
+      name: 'Pf-Changs',
+      time: '35-45 min',
+      price: '$$',
+      review: 762,
+      rating: 4.7,
+    },
+    {
+      id: 124,
+      image: 'https://cdn.pixabay.com/photo/2021/07/19/16/04/pizza-6478478__480.jpg',
+      name: 'Pizzeria Victor',
+      time: '25-30 min',
+      price: '$$',
+      review: 438,
+      rating: 4.4,
+    },
+    {
+      id: 125,
+      image: 'https://cdn.pixabay.com/photo/2018/08/03/08/33/food-3581341__480.jpg',
+      name: 'Sushi Club',
+      time: '30-40 min',
+      price: '$$',
+      review: 292,
+      rating: 4.5,
+    },
+  ]
+
   const items = [
-    {type: 'CategoriesList'},
-    {type: 'RestaurantItem'},
+    {key: 1, type: 'CategoriesList'},
+    {key: 2, type: 'RestaurantItem', payload: restaurants},
   ]
 
   const _renderItem = (item) => {
     if (item.type === 'CategoriesList') {
       return (
-        <View>
+        <View style={{backgroundColor: '#fff', paddingVertical: RFValue(10)}}>
           <CategoriesList/>
         </View>
       )
     }
     if (item.type === 'RestaurantItem') {
       return (
-        <View>
-          <RestaurantItem/>
+        item.payload.map((restaurant, index) => (
+          <View key={restaurant.id} style={{marginTop: RFValue(10)}}>
+          <RestaurantItem info={restaurant}/>
         </View>
+        ))
       )
     }
   }
@@ -37,7 +70,7 @@ const HomeScreen = () => {
       </View>
       <FlatList
         data={items}
-        keyExtractor={({index}) => index}
+        keyExtractor={(item, index) => item.key}
         showsVerticalScrollIndicator={false}
         renderItem={({item}) => _renderItem(item)}
       />
@@ -50,9 +83,10 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
   },
   topContainer: {
-    marginBottom: RFValue(15),
+    backgroundColor: '#fff',
+    paddingBottom: RFValue(10)
   },
 })
